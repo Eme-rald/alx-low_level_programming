8c9977e95a973d90b981dcf0545da19063139ece
Eme-rald <adiliadaeze@gmail.com>
2022-09-13 14:04:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"                                                                                                               
-                                                                                                                                
-/**                                                                                                                             
- * main - prints putchar                                                                                                        
- * prints _putchar followed by a new line                                                                                       
- * Return: always 0                                                                                                             
- */                                                                                                                             
-int main(void)                                                                                                                  
-{                                                                                                                
-	_putchar('_');                                                                                                          
-	_putchar('p');                                                                                   
-	_putchar('u');                                                                                          
-	_putchar('t');                                                                                                      
-	_putchar('c');                                                                                      
-	_putchar('h');                                                                                             
-	_putchar('a');                                                                                             
-	_putchar('r');                                                                                                        
-	_putchar('\n');                                                                                          
-	return (0);                                                                                                             
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
