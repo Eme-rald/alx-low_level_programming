212d2c84112721a289255815c22592694b7e9110
Eme-rald <adiliadaeze@gmail.com>
2022-09-14 03:00:48 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and print + if n is greater than zero
+ * Return: 1 and print + if n is greater than zer0
  * 0 and prints 0 if n is zero
  * -1 and prints 0 if n is less than zero
  */
@@ -20,7 +20,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else if (n <)
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
