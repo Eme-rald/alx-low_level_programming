d093a517578c9973793731087e6ce69b6947f2e1
Eme-rald <adiliadaeze@gmail.com>
2022-09-22 05:07:32 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer
+ * print_buffer - prints buffer
  * @b: buffer
  * @size: size
  * Return: void
@@ -21,7 +21,7 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - o < 10 ? aize - o : 10;
+		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
