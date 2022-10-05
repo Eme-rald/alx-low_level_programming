8be6f472d8bda993504b13d99311e631bba14918
Eme-rald <adiliadaeze@gmail.com>
2022-10-05 00:59:14 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	num = atoi(argv[1]);
 	result = 0;
 
-	if(num < 0)
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 		while (num >= coins[j])
 		{
 			result++;
-			num-= coins[j];
+			num -= coins[j];
 		}
 	}
 
