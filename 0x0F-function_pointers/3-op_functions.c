afb70997dd8e09d9321e9e21b910a455312eb00c
Eme-rald <adiliadaeze@gmail.com>
2022-10-12 14:32:39 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,8 +11,19 @@ int op_add(int a, int b)
 
 /**
  * op_sub - compute difference of two ints
+ * @a: int to add to b
+ * @b: int to add to a
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - compute difference of two ints
  * @a: int to subtract with b
- * @b: int to subtract from a 
+ * @b: int to subtract from a
  * Return: difference of a and b
  */
 int op_sub(int a, int b)
@@ -34,7 +45,7 @@ int op_mul(int a, int b)
 /**
  * op_div - compute quotient of two ints
  * @a: int to divide by b
- * @b: int to divide by a
+ * @b: int to divide a by
  * Return: quotient of a and b
  */
 int op_div(int a, int b)
@@ -46,7 +57,7 @@ int op_div(int a, int b)
  * op_mod - compute remainder of two ints
  * @a: int to mod with b
  * @b: int to mod a by
- * Return: remainder bof a and b
+ * Return: remainder of a and b
  */
 int op_mod(int a, int b)
 {
