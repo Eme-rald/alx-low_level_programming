343d470a7b033005c125446efb713377029a2015
Eme-rald <adiliadaeze@gmail.com>
2022-10-12 11:58:06 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,8 @@
  * print_name - prints name
  * @name: name to print
  * @f: function that prints name
- */void print_name(char *name, void (*f)(char *))
+ */
+void print_name(char *name, void (*f)(char *))
 {
 	if (f)
 		f(name);
