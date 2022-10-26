26221c13a62076c9152689cdb58ac625f8732ebf
Eme-rald <adiliadaeze@gmail.com>
2022-10-26 04:44:05 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,20 +6,17 @@
  *
  * Return: no return.
  */
-
 void free_listp(listp_t **head)
 {
 	listp_t *temp;
 	listp_t *curr;
-
+	
 	if (head != NULL)
 	{
 		curr = *head;
-
 		while ((temp = curr) != NULL)
 		{
 			curr = curr->next;
-
 			free(temp);
 		}
 		*head = NULL;
@@ -32,42 +29,42 @@ void free_listp(listp_t **head)
  *
  * Return: number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
-
+	
 	hptr = NULL;
 	while (head != NULL)
 	{
-		new = malloc(sizeof(lisp_t));
-
+		new = malloc(sizeof(listp_t));
+		
 		if (new == NULL)
 			exit(98);
-
+		
 		new->p = (void *)head;
 		new->next = hptr;
 		hptr = new;
-
+		
 		add = hptr;
-
+		
 		while (add->next != NULL)
 		{
 			add = add->next;
 			if (head == add->p)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
+				printf("-> [%p] %d\n", (void *)head, head->n)
 				free_listp(&hptr);
 				return (nnodes);
+			
 			}
 		}
-
+		
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		nnodes++;
 	}
-
+	
 	free_listp(&hptr);
 	return (nnodes);
 }
